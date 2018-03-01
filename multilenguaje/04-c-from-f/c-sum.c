b80c2ef94a2a9f38f0d2fe06d52e3942a7590fa6
EOTMB <agustinmorelli88@gmail.com>
2018-03-01 14:09:25 +0100

    Makefile

--- multilenguaje/04-c-from-f/c-sum.c
+++ multilenguaje/04-c-from-f/c-sum.c
@@ -4,8 +4,8 @@
 void sum_abs_(int *data, int *n,int *asum) {
    int i;
 
-   for (i=0,*asum=0; i < n; ++i) {
-       asum += abs(data[i]);
+   for (i=0, *asum=0; i < *n; ++i) {
+       *asum += abs(data[i]);
    }
 }
  
--- multilenguaje/04-c-from-f/c-sum.h
+++ multilenguaje/04-c-from-f/c-sum.h
@@ -2,6 +2,6 @@
 #ifndef C_SUM_H
 #define C_SUM_H
 
-extern void sum_abs_(int *, int *, int *);
+extern void sum_abs_(int *data, int *num, int *asum);
 
 #endif
